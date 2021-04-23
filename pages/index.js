9194215ce259b6cbdd7e7439376297ae0f62f43e
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-23 18:34:57 +0530

    updated 8

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+       <Head>
+        <title>Lifestyle News: Latest Lifestyle</title>
       </Head>
 
       <main className={styles.main}>
