348e2f85e9008a70c0194e8c89234d21df95a74d
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-26 19:27:16 +0530

    api testing

--- pages/fashion/index.js
+++ pages/fashion/index.js
@@ -8,21 +8,18 @@ const short_Ads = './assets/images/300x250.png';
 const backArrowImg = './assets/images/backarrow.png';
 const add_img = './assets/images/add.png';
 
-export default function Fashion({fashionData}){
-    console.log("fashionData -", fashionData);
-   return(
-    <>
-    <Head>
-        <title>Fashion News: Latest Fashion</title>
-    </Head>
-        {/* Add Section */}
-        <div className="add_wrapper">
-                    <img src={add_img} alt="add" />
-        </div>
-    <h1>Fashion is working</h1>
-        </>
-   )
-}
+class myComponent extends React.Component {
+    componentDidMount() {
+      const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => console.log('This is your data', data));
+    }
+    render() {
+      return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+    }
+  }
+  export default myComponent;
 
 // export async function getStaticProps(){
 //     const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=fashion&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all`)
@@ -33,10 +30,10 @@ export default function Fashion({fashionData}){
 // }
 
 
-export async function getServerSideProps(){
-    const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=fashion&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all`)
-    const fashionData = await res.json()
-    return {
-        props: { fashionData, },
-      };
-}
+// export async function getServerSideProps(){
+//     const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=fashion&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all`)
+//     const fashionData = await res.json()
+//     return {
+//         props: { fashionData, },
+//       };
+// }
