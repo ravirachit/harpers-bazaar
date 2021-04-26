51ecafc4158318c0142b6a0dce0d8c1957832164
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-26 13:35:48 +0530

    just check

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const FashionAdd = './assets/images/fashionadd.png';
 const cultureAdImg = './assets/images/300x250.png';
 
 export default function Home({homeData}){
-    console.log("homeData- ", homeData);
+    // console.log("homeData- ", homeData);
     return(
        <>
         <Head>
@@ -19,6 +19,8 @@ export default function Home({homeData}){
                     <img src={add_img} alt="add" />
         </div>
         {/* <div>{homeData}</div> */}
+
+        <h1>Home page is working</h1>
            
             </>
     )
@@ -32,14 +34,14 @@ export default function Home({homeData}){
 //       };
 // }
 
-export async function getServerSideProps(){
-    const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/template?name=home`,function (req, res) {
-        res.header("Access-Control-Allow-Origin", "http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/template?name=home")})
-    const homeData = await res.json()
-    return {
-        props: { homeData, },
-      };
-}
+// export async function getServerSideProps(){
+//     const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/template?name=home`,function (req, res) {
+//         res.header("Access-Control-Allow-Origin", "http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/template?name=home")})
+//     const homeData = await res.json()
+//     return {
+//         props: { homeData, },
+//       };
+// }
 
 // Home.getInitialProps = async()=>{
 //     const res = await fetch(`https://reqres.in/api/users`)
--- pages/travelAndFood/index.js
+++ pages/travelAndFood/index.js
@@ -6,6 +6,7 @@ import Head from 'next/head';
 const Ads = './assets/images/add.png';
 const short_Ads = './assets/images/300x250.png';
 const backArrowImg = './assets/images/backarrow.png';
+const add_img = './assets/images/add.png';
 
 export default function TravelAndFood({travelAndFoodData}){
    return(
@@ -13,71 +14,11 @@ export default function TravelAndFood({travelAndFoodData}){
     <Head>
         <title>Travel News: Latest Travel news</title>
     </Head>
-    {/* ads section */}
-    <div className="add_wrapper">
-            <img src={Ads} alt="ad" />
-    </div>
-
-    {/* Navigation */}
-    <div class="nevigation_section">
-            <div class="container">
-                <a href="www.facebook.com" class="active">Home  </a>
-                <span> <img src={backArrowImg} alt="back" /></span>
-                <a href="www.facebook.com">{travelAndFoodData.data.feed[0].primary_section_name}</a>
-            </div>
+        {/* Add Section */}
+        <div className="add_wrapper">
+                    <img src={add_img} alt="add" />
         </div>
-
-         {/* Heading */}
-         <h1 className="tittle_text"><span>{travelAndFoodData.data.feed[0].primary_section_name}</span></h1>
-
-         <section class="content_wrapper">
-                <div class="container">
-                    <div class="content_inner">
-                        <div class="content_left">
-            
-                        {/* Fashion */}     
-                        <div class="fashion_top">
-                            <figure>
-                            <img src={travelAndFoodData.data.feed[0].feature_image_data.url} alt="fashion_img_main" />
-                            </figure>
-                            <Link href={'/story/'+travelAndFoodData.data.feed[0].url.slice(travelAndFoodData.data.feed[0].url.length - 4)}><h3>{travelAndFoodData.data.feed[0].headline}</h3></Link>
-                        </div> 
-                        </div>	                                  	
-            <div class="content_right">
-                {/* Small ADS */}
-            <figure class="add_box">
-                 <img src={short_Ads} alt="short-ads-img" />
-            </figure>
-
-            {/* rhs story */}
-            <h4>Must Read</h4>
-            <div class="right_card">
-                <figure><img src={travelAndFoodData.data.feed[1].feature_image_data.url} alt="rhs_img" /></figure>
-                <p>{travelAndFoodData.data.feed[1].headline}</p>
-            </div>
-			</div>
-            </div>
-            <ul class="section_list">
-            {
-                travelAndFoodData.data.feed.slice(2).map((travelAndFood)=>{
-                    return(
-                        <li>
-                        <div class="list_detail">
-                            <strong>{moment(travelAndFood.updated_at).format("MMM DD, YYYY")}</strong>
-                            <h3><a href={travelAndFood.url}>{travelAndFood.headline}</a></h3>
-                            <p>{travelAndFood.introtext}</p>
-                            <span>By <a href="/authors/bazaar-india">{travelAndFood.author_data[0].author_name}</a></span>
-                        </div>
-                        <figure class="list_pic">
-                            <img src={travelAndFood.feature_image_data.url} alt="list_img1"/>
-                        </figure>
-                    </li> 
-                    )
-                })
-            }
-				 </ul>
-            </div>
-		</section>
+    <h1>Travel And Food is working</h1>
         </>
    )
 }
@@ -90,10 +31,10 @@ export default function TravelAndFood({travelAndFoodData}){
 //       };
 // }
 
-export async function getServerSideProps(){
-    const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=travel-food&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all`)
-    const travelAndFoodData = await res.json()
-    return {
-        props: { travelAndFoodData, },
-      };
-}
+// export async function getServerSideProps(){
+//     const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=travel-food&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all`)
+//     const travelAndFoodData = await res.json()
+//     return {
+//         props: { travelAndFoodData, },
+//       };
+// }
