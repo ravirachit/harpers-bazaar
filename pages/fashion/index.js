9c852b9d6f9a8b06e672cab18481f06e515cdc6d
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-26 19:30:23 +0530

    api test 1

--- pages/fashion/index.js
+++ pages/fashion/index.js
@@ -10,7 +10,7 @@ const add_img = './assets/images/add.png';
 
 class myComponent extends React.Component {
     componentDidMount() {
-      const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+      const apiUrl = 'http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=fashion&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all';
       fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => console.log('This is your data', data));
