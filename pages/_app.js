8b8d720b5dfcc51522874877a84a58aebab7f4e0
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-26 12:59:08 +0530

    layout test 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import '../styles/common.css';
+import Header from './header';
+import Footer from './header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Header/>
+      <Component {...pageProps} />
+    <Footer/>
+    </>
+  )
 }
 
 export default MyApp
