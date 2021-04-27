911ecc4facca106805534833f26eb274de3940c1
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-27 17:00:13 +0530

    just check local api

---
+++ data.js
@@ -0,0 +1,12 @@
+export const jobs = [
+    {
+        id:1,
+        name:'Ravi Kumar',
+        title:"Developer"
+    },
+    {
+        id:2,
+        name:'Rachit Rathore',
+        title:"Kids"
+    }
+]
---
+++ pages/api/jobs/index.js
@@ -0,0 +1,6 @@
+// import jobs from '../../../data.js'
+
+export default async (req, res) => {
+    res.status(200).json({name:"ravi kumar"})
+  }
+  
