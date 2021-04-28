03c2504808dbca7e44375a33cd508cb5360ed054
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-28 11:49:31 +0530

    just check api

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export const jobs = [
     {
         id:1,
@@ -11,3 +14,11 @@ export const jobs = [
     }
 ]
 
+// useEffect(()=>{
+//     async function getHomeData(){
+//         const res = await axios.get('http://dev-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/template?name=home');
+//         console.log("res- ", res);
+//         setHomedata(res.data.data.feed);
+//     }
+//     getHomeData();
+// },[]);
--- pages/api/jobs/index.js
+++ pages/api/jobs/index.js
@@ -1,6 +1,12 @@
+
 import {jobs} from '../../../data'
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
 
 export default async (req, res) => {
-    res.status(200).json(jobs)
-  }
+  const resData = await axios.get('http://dev-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=fashion&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all');
+  // console.log("res- ", resData.body);
+  // res.send(resData.json());
+    res.status(200).json(resData.data)
+}
   
