a7e30d72ef5af98272da726cd342060244a2475d
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-29 14:44:11 +0530

    check next api with static json

--- pages/api/jobs/index.js
+++ pages/api/jobs/index.js
@@ -4,9 +4,9 @@ import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 
 export default async (req, res) => {
-  const resData = await axios.get('http://dev-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=fashion&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all');
+  // const resData = await axios.get('http://dev-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/latest?section=fashion&section_level=0content_type=all&page=1&limit=10&order_by=publish_up%20desc&app=all');
   // console.log("res- ", resData.body);
   // res.send(resData.json());
-    res.status(200).json(resData.data)
+    res.status(200).json(jobs)
 }
   
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+
 import React from 'react';
+import Link from 'next/link';
 import Head from 'next/head';
 
-// import Ad from 'react-google-publisher-tag';
 import {Bling as GPT} from "react-gpt";
 
 GPT.enableSingleRequest();
+
 // Images
 // const banner_img = './assets/images/banner.png';
 const add_img = './assets/images/add.png';
@@ -12,64 +14,280 @@ const FashionAdd = './assets/images/fashionadd.png';
 const cultureAdImg = './assets/images/300x250.png';
 
 export default function Home({homeData}){
- 
     console.log("homeData- ", homeData);
     return(
        <>
-       <div id="div-gpt-ad-6326067-1">
+        <Head>
+        <title>Lifestyle News: Latest Lifestyle</title>
+        <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
+      </Head>
+            {/* Add Section */}
+         <div className="add_wrapper">
+         <div id="div-gpt-ad-6326067-1">
                 <GPT
                     adUnitPath="/1007232/HarperBazaar_HP_300x250-728x90_ATF"
                     slotSize={[728, 90]}
                 />
             </div>
-            
-       {/* <div id="div-gpt-ad-6326067-1">
-        <Ad path="/1007232/HarperBazaar_HP_300x250-728x90_ATF" />
-      </div> */}
-        <Head>
-        <title>Lifestyle News: Latest Lifestyle</title>
-        {/* <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script> */}
-      </Head>
-            {/* Add Section */}
-         <div className="add_wrapper">
-         <div id="div-gpt-ad-6326067-2">
+        </div>
+           
+             {/* Slider */}
+           <section className="banner">
+              <div className="container">
+                   <figure>
+                        <img src={homeData.data.feed.templatedata[0].content_data.widget_data.widget_data.main_data[0].feature_image_data.url} alt="banner"/>
+                   </figure>
+                   <h2>{homeData.data.feed.templatedata[0].content_data.widget_data.widget_data.main_data[0].headline}</h2>
+              </div>
+        </section>
+
+
+        {/* Fashion Section */}
+<section className="fashion">
+            <h1 className="tittle_text"><span>{homeData.data.feed.templatedata[2].content_data.title}</span></h1>
+                <div className="container">
+                    <div className="fashion_wrap">
+                        <div className="fashion_cell">
+                            <figure>
+                                <img src={homeData.data.feed.templatedata[2].content_data.widget_data.widget_data.main_data[0].feature_image_data.url} alt="fashion1" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[2].content_data.widget_data.widget_data.main_data[0].headline}
+                            </p>
+                        </div>
+                        <div className="fashion_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[2].content_data.widget_data.widget_data.main_data[1].feature_image_data.url} alt="fashion1" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[2].content_data.widget_data.widget_data.main_data[1].headline}
+                            </p>
+                        </div>
+                        <div className="fashion_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[2].content_data.widget_data.widget_data.main_data[2].feature_image_data.url} alt="fashion1" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[2].content_data.widget_data.widget_data.main_data[2].headline}
+                            </p>
+                        </div>
+                        <div className="fashion_cell">
+                            <figure className="add_fashion">
+                                <img src={FashionAdd} alt="fashionadd" />
+                            </figure>
+                            <figure>
+                                <img src={homeData.data.feed.templatedata[2].content_data.widget_data.widget_data.main_data[3].feature_image_data.url} alt="fashion1" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[2].content_data.widget_data.widget_data.main_data[3].headline}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section> 
+            {/* end */}
+
+             {/* Beauty Section */}
+             <section className="travel_food">
+            <h1 className="tittle_text"><span>{homeData.data.feed.templatedata[3].content_data.title}</span></h1>
+                <div className="container">
+                    <div className="travel_inner">
+                        <div className="travel_left">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[3].content_data.widget_data.widget_data.main_data[0].feature_image_data.url} alt="Beauty" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[3].content_data.widget_data.widget_data.main_data[0].headline}
+                            </p>
+                        </div>
+                        <div className="travel_right">
+                            <ul className="travel_list">
+                                <li>
+                                <figure>
+                                <img src={homeData.data.feed.templatedata[3].content_data.widget_data.widget_data.main_data[1].feature_image_data.url} alt="Beauty1" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[3].content_data.widget_data.widget_data.main_data[1].headline}
+                            </p>
+                                </li>
+                                <li>
+                                <figure>
+                                <img src={homeData.data.feed.templatedata[3].content_data.widget_data.widget_data.main_data[2].feature_image_data.url} alt="Beauty2" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[3].content_data.widget_data.widget_data.main_data[2].headline}
+                            </p>
+                                </li>
+                                <li>
+                                <figure>
+                                <img src={homeData.data.feed.templatedata[3].content_data.widget_data.widget_data.main_data[3].feature_image_data.url} alt="Beauty3" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[3].content_data.widget_data.widget_data.main_data[3].headline}
+                            </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* end */}
+
+             {/* Culture Section */}
+             <section className="culture">
+            <h1 className="tittle_text"><span>{homeData.data.feed.templatedata[4].content_data.title}</span></h1>
+                <div className="container">
+                    <div className="culture_top_sec">
+                        <div className="culture_top_left">
+                            <div className="culture_img">
+                            <img src={homeData.data.feed.templatedata[4].content_data.widget_data.widget_data.main_data[0].feature_image_data.url} alt="culture" />
+                            </div>
+                            <div className="culture_text">
+                                <p>{homeData.data.feed.templatedata[4].content_data.widget_data.widget_data.main_data[0].headline}</p>
+                            </div>
+                        </div>
+                        <div className="add_sec">
+                        <div id="div-gpt-ad-6326067-2">
                 <GPT
                     adUnitPath="/1007232/HarperBazaar_HP_300x250_MTF"
                     slotSize={[300, 250]}
                 />
             </div>
-        </div>
+                        </div>
+                    </div>
+                    <div className="culture_list">
+                        <div className="culture_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[4].content_data.widget_data.widget_data.main_data[1].feature_image_data.url} alt="culture1" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[4].content_data.widget_data.widget_data.main_data[1].headline}
+                            </p>
+                        </div>
+                        <div className="culture_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[4].content_data.widget_data.widget_data.main_data[2].feature_image_data.url} alt="culture2" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[4].content_data.widget_data.widget_data.main_data[2].headline}
+                            </p>
+                        </div>
+                        <div className="culture_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[4].content_data.widget_data.widget_data.main_data[3].feature_image_data.url} alt="culture3" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[4].content_data.widget_data.widget_data.main_data[3].headline}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* end */}
 
-        <div id="div-gpt-ad-6326067-3">
+            {/* Celebrity Section */}
+            <section className="fashion Celebrity">
+            <h1 className="tittle_text"><span>{homeData.data.feed.templatedata[5].content_data.title}</span></h1>
+                <div className="container">
+                    <div className="fashion_wrap">
+                        <div className="fashion_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[5].content_data.widget_data.widget_data.main_data[0].feature_image_data.url} alt="celebrity1" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[5].content_data.widget_data.widget_data.main_data[0].headline}
+                            </p>
+                        </div>
+                        <div className="fashion_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[5].content_data.widget_data.widget_data.main_data[1].feature_image_data.url} alt="celebrity2" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[5].content_data.widget_data.widget_data.main_data[1].headline}
+                            </p>
+                        </div>
+                        <div className="fashion_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[5].content_data.widget_data.widget_data.main_data[2].feature_image_data.url} alt="celebrity3" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[5].content_data.widget_data.widget_data.main_data[2].headline}
+                            </p>
+                        </div>
+                        <div className="fashion_cell">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[5].content_data.widget_data.widget_data.main_data[3].feature_image_data.url} alt="celebrity4" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[5].content_data.widget_data.widget_data.main_data[3].headline}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* end */}
+
+          {/* Travel & Food Section */}
+          <section className="travel_food">
+            <h1 className="tittle_text"><span>{homeData.data.feed.templatedata[6].content_data.title}</span></h1>
+                <div className="container">
+                    <div className="travel_inner">
+                        <div className="travel_left">
+                        <figure>
+                                <img src={homeData.data.feed.templatedata[6].content_data.widget_data.widget_data.main_data[0].feature_image_data.url} alt="travelAndFood1" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[6].content_data.widget_data.widget_data.main_data[0].headline}
+                            </p>
+                        </div>
+                        <div className="travel_right">
+                            <ul className="travel_list">
+                                <li>
+                                <figure>
+                                <img src={homeData.data.feed.templatedata[6].content_data.widget_data.widget_data.main_data[1].feature_image_data.url} alt="travelAndFood2" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[6].content_data.widget_data.widget_data.main_data[1].headline}
+                            </p>
+                                </li>
+                                <li>
+                                <figure>
+                                <img src={homeData.data.feed.templatedata[6].content_data.widget_data.widget_data.main_data[2].feature_image_data.url} alt="travelAndFood3" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[6].content_data.widget_data.widget_data.main_data[2].headline}
+                            </p>
+                                </li>
+                                <li>
+                                <figure>
+                                <img src={homeData.data.feed.templatedata[6].content_data.widget_data.widget_data.main_data[3].feature_image_data.url} alt="travelAndFood4" />
+                            </figure>
+                            <p>
+                                {homeData.data.feed.templatedata[6].content_data.widget_data.widget_data.main_data[3].headline}
+                            </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div className="add_wrapper">
+            <div id="div-gpt-ad-6326067-3">
                 <GPT
                     adUnitPath="/1007232/HarperBazaar_HP_728x90-300x250_BTF"
                     slotSize={[800, 450]}
                 />
             </div>
-       
+        </div>
 
         
-        {/* <div>{homeData}</div> */}
 
-        <h1>Home page is working</h1>
-
-        {/* <div id="div-gpt-ad-6326067-2">
-        <Ad path="/1007232/HarperBazaar_HP_300x250_MTF" />
-      </div> */}
-           
             </>
     )
 }
 
-// export async function getStaticProps(){
-//     const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/template?name=home`)
-//     const homeData = await res.json()
-//     return {
-//         props: { homeData, },
-//       };
-// }
-
-export async function getServerSideProps(){
+export async function getStaticProps(){
     const res = await fetch(`https://harpers-bazaar.vercel.app/api/jobs`)
     const homeData = await res.json()
     return {
@@ -77,8 +295,10 @@ export async function getServerSideProps(){
       };
 }
 
-// Home.getInitialProps = async()=>{
-//     const res = await fetch(`https://reqres.in/api/users`)
+// export async function getServerSideProps(){
+//     const res = await fetch(`http://alpha-cms-chub-harperbazar.simpleapi.itgd.in/feeds/v3c/template?name=home`)
 //     const homeData = await res.json()
-//     return {homeData:homeData};
+//     return {
+//         props: { homeData, },
+//       };
 // }
