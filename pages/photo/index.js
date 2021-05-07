0a38bb1bac177c2fa3cf660521864e810d0f05ad
unknown <admin@NODFC20LT210.tvtoday.net>
2021-05-07 16:03:44 +0530

    ads testing with ad npm 1

--- pages/photo/index.js
+++ pages/photo/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import Ad from 'react-google-publisher-tag';
 
+Ad.enableSingleRequest = true;
+
 export default class Example extends Component {
   render() {
     return (
         <>
       <div id="div-gpt-ad-6326067-1">
-        <Ad path="/1007232/HarperBazaar_HP_300x250-728x90_ATF" />
+        <Ad path="/1007232/HarperBazaar_HP_300x250-728x90_ATF" enableSingleRequest={true}/>
         {/* <RouteHandler /> */}
       </div>
        <div id="div-gpt-ad-6326067-2">
-       <Ad path="/1007232/HarperBazaar_HP_300x250_MTF" />
+       <Ad path="/1007232/HarperBazaar_HP_300x250_MTF" enableSingleRequest={true}/>
        {/* <RouteHandler /> */}
      </div>
      </>
