502fb8d58f21d2df7deb6eccaf43a1559d062acd
unknown <admin@NODFC20LT210.tvtoday.net>
2021-05-07 13:02:12 +0530

    ads testing

--- pages/photo/index.js
+++ pages/photo/index.js
@@ -1,5 +1,36 @@
+import React, { Component } from 'react';
+import { Gpt } from 'react-gpt-ads';
+import Head from 'next/head';
+
 export default function Photo(){
     return(
+        <>
+         <Head>
+        {/* <title>Fashion News: Latest Fashion</title> */}
+        <script async src='https://securepubads.g.doubleclick.net/tag/js/gpt.js'></script>
+    </Head>
         <div>Photo is working</div>
+        <Gpt 
+        adUnit="/1007232/HarperBazaar_HP_300x250-728x90_ATF"
+        name="ad-name"
+        size={[300, 250]}
+    />
+    </>
     )
 }
+
+
+// import React, { Component } from 'react';
+// import Ad from 'react-google-publisher-tag';
+
+// export default class Photo extends Component {
+//   render() {
+//     return (
+//       <div id="application">
+//         <Ad path="/network-code/ad-code" />
+//         <RouteHandler />
+//       </div>
+//     );
+//   }
+// }
+
